faba858b5768d57533d65c4250c65ad00512d851
Alexander Myagkov <anistonbale@gmail.com>
2022-11-21 22:21:58 +0300

    task1

---
+++ Begining.py
@@ -0,0 +1,32 @@
+# num1=int(input("Введите 1 число: "))
+# num2=int(input("Введите 2 число: "))
+# if num2==num1**2:
+#     print("Да. является")
+# elif num1==num2**2:
+#     print("Да. является")
+# else:
+#     print("Нет, не является")
+# Напишите программу, которая на вход принимает 5 чисел и находит максимальное из них.
+# 1, 4, 8, 7, 5 -> 8
+
+num1 = int(input("Введите число: "))
+num2 = int(input("Введите число: "))
+num3 = int(input("Введите число: "))
+num4 = int(input("Введите число: "))
+num5 = int(input("Введите число: "))
+list = []
+list.append(num1)
+list.append(num2)
+list.append(num3)
+list.append(num4)
+list.append(num5)
+max_number=list[0]
+for i in range(0,5):
+    if list[i]>max_number:
+        max_number=list[i]
+# if num2>max_number:
+#     max_number=num2
+print(max_number)
+
+
+
