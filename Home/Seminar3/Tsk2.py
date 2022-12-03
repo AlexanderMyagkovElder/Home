882aefc69b43104196384ddd61fc6b824ec78bf1
Alexander Myagkov <anistonbale@gmail.com>
2022-12-03 12:26:38 +0300

    re2 task

--- Home/Seminar3/Tsk2.py
+++ Home/Seminar3/Tsk2.py
@@ -1,8 +1,13 @@
-# 3.11 Вводим с клавиатуры строку. Необходимо сказать, является ли эта строка дробным числом
+#3.11 Вводим с клавиатуры строку. Необходимо сказать, является ли эта строка дробным числом
 
-string_checking = input('Enter fractional number: ')
-print(string_checking.isnumeric)
-if string_checking == int(string_checking):
-    print('False')
-else:
-    print('True')
+while True:
+    try:
+        string_checking=float(input('Enter fractional number: '))
+        if string_checking==int(string_checking):
+            print('False')
+        else:
+            print('True')
+    except ValueError:
+        print("it's not fractional number!")
+    else:
+        break
