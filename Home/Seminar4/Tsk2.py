85729115d2796b6ab619399c26032a8608846073
Alexander Myagkov <anistonbale@gmail.com>
2022-12-07 22:11:14 +0300

    sem4

--- Home/Seminar4/Tsk2.py
+++ Home/Seminar4/Tsk2.py
@@ -5,16 +5,12 @@ import random
 a = int(input('Enter the numbers of rows: '))
 b = int(input(('Enter the numbers of columns: ')))
 array1 = []
-for i in range(a-1):
-    for j in range(b-1):
-        array1[i][j]=0
-# sum_rows = []
-# sum_1=0
-# for i in range(b):
-#     for j in range(a):
-#         array1.append(int(random.randint(1, 10)))
-#         sum_1 += array1[j]
-#     sum_rows.append(sum_1)
-#     sum_1 = 0
+sum_rows = []
+for i in range(b):
+    sum_1 = 0
+    for j in range(a):
+        array1.append(int(random.randint(1, 10)))
+        sum_1 += array1[j]
+    sum_rows.append(sum_1)
 print(array1)
-# print(sum_rows)
+print(sum_rows)
